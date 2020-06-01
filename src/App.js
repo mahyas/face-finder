4a9c67529bf0860f3f99bad129cb14ce5253d3fe
mahyas <mahy.sadeghi@gmail.com>
2020-06-01 10:49:31 -0700

    added FaceFinder API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceFinder from './components/FaceFinder/FaceFinder.js';
 import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkInput from './components/ImageLinkInput/ImageLinkInput.js';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '2a7601a0f2b7488189316c04b9738561',
+})
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 90,
+      density: {
+        enable: true,
+        value_area : 600
+      }
+    }
+  }
+}
 
-class App extends component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input:'',
+      imageUrl:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSumbit = () => {
+    this.setState({imageUrl:this.state.input});
+    app.models.predict(
+      Clarifai.COLOR_MODEL,
+      this.state.input)
+      .then(
+      function(response) {
+        console.log(response)
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+        params={particlesOptions}
+        />
         <Navigation />
-        {/* <Logo />
-        <ImageLinkInput />
-        <FaceRecognition /> */}
+        <Logo />
+        <Rank />
+        <ImageLinkInput onInputChange={this.onInputChange} 
+                        onButtonSubmit={this.onButtonSumbit}/>
+        <FaceFinder imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
---
+++ src/components/FaceFinder/FaceFinder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FaceFinder = ({imageUrl}) =>{
+    return(
+        <div className='center'>
+            <img alt='' src={imageUrl} />
+        </div>
+        
+    )
+}
+
+export default FaceFinder;
