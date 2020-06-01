09f841643f0ddcf83283f462d6cabcbf10e1042f
mahyas <mahy.sadeghi@gmail.com>
2020-06-01 14:05:51 -0700

    added Face box

--- src/App.js
+++ src/App.js
@@ -29,7 +29,25 @@ class App extends Component {
     this.state = {
       input:'',
       imageUrl:'',
+      box:{},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+  
+  displayFaceBox = (box) => {
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -37,18 +55,12 @@ class App extends Component {
   }
 
   onButtonSumbit = () => {
-    this.setState({imageUrl:this.state.input});
+    this.setState({imageUrl: this.state.input});
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response) {
-        console.log(response)
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -62,7 +74,7 @@ class App extends Component {
         <Rank />
         <ImageLinkInput onInputChange={this.onInputChange} 
                         onButtonSubmit={this.onButtonSumbit}/>
-        <FaceFinder imageUrl={this.state.imageUrl}/> 
+        <FaceFinder box={this.state.box} imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () =>{
     return(
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> <img style={{paddingTop:'15px'}} src="http://emahya.com/wp-content/uploads/2018/01/board-2181ff407_1920.png"></img> </div>
+                <div className="Tilt-inner pa3"> <img alt='' style={{paddingTop:'15px'}} src="http://emahya.com/wp-content/uploads/2018/01/board-2181ff407_1920.png"></img> </div>
             </Tilt>
         </div>
     )
