175b02d6c6409e85365604a8bc677c7ef28d9cb8
mahyas <mahy.sadeghi@gmail.com>
2020-06-02 17:11:07 -0700

    added signin connection

--- facefinder-api/server.js
+++ facefinder-api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -52,6 +55,7 @@ app.post('/register', (req, res) => {
         entries: 0,
         joined: new Date()
     })
+
     res.json(database.users[database.users.length-1]);
 })
 
@@ -68,6 +72,22 @@ app.get('/profile/:id', (req, res) =>{
             res.status(400).json('no such user')
     }
 })
+
+app.put('/image', (req, res) =>{
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user =>{
+        if(user.id === id){
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    })
+    if (!found){
+            res.status(400).json('no such user')
+    }
+})
+
 
 app.listen(3000, () => {
     console.log('app is running on port 3000')
