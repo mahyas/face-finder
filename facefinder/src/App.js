5850fb9a2a3a5fc3f7ea9324e5f8d5321e42d25c
mahyas <mahy.sadeghi@gmail.com>
2020-06-10 22:33:25 -0700

    added controllers

--- facefinder/src/App.js
+++ facefinder/src/App.js
@@ -26,23 +26,26 @@ const particlesOptions = {
   }
 }
 
+const initialState= {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    
+}
+
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState
   }
 
   loadUser = (data) => {
@@ -104,7 +107,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
